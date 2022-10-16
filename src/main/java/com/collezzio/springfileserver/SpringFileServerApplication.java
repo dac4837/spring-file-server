7280a7d31d96f1f7d20c404a9e9b2d1b27de2efa
David Collins <DavidCollins@Catherines-MacBook-Pro.local>
2022-10-16 09:13:55 -0400

    initial commit

---
+++ src/main/java/com/collezzio/springfileserver/SpringFileServerApplication.java
@@ -0,0 +1,13 @@
+package com.collezzio.springfileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFileServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collezzio/springfileserver/SpringFileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collezzio.springfileserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFileServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
